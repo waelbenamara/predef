db853f4475fac6ce437bd959fda6808543bac7e8
moetezz <moetez.com@gmail.com>
2021-04-04 23:01:20 -0400

    AnalyserUpdate

--- Analyser.py
+++ Analyser.py
@@ -1,11 +1,38 @@
-from radon.complexity import cc_visit
+from radon.complexity import average_complexity, cc_visit
+from radon.raw import analyze
 
 
-filename = "test.py"
+def cyclomaticComplexity(filename):
+    with open(filename) as fobj:
+        source = fobj.read()
+        cc = cc_visit(source)
+        return average_complexity(cc)
 
-with open(filename) as fobj:
-	source = fobj.read()
 
-cc = cc_visit(source)
-print(cc[0].complexity)
+def generateRawMetrics(filename):
+	with open(filename) as fobj:
+         source = fobj.read()
+	x = analyze(source)
 
+def numberOfOperators(filename):
+    with open(filename) as fobj:
+        source = fobj.read()
+    x = analyze(source)
+    return x.loc
+
+# def numberOfOperands():
+
+# def totalNumberOfOperators():
+
+# def totalNumberOfOperands():
+
+# def halsteadProgramLength():
+
+# def linesOfCode():
+
+# def linesOfComments():
+
+
+if __name__ == "__main__":
+    print(numberOfOperators("test.py"))
+    print(cyclomaticComplexity("test.py"))
--- fileExplorer.py
+++ fileExplorer.py
@@ -1,13 +1,14 @@
 import os
 
 def exploreFiles(rootPath):
+    filesList = []
     for file in os.listdir(rootPath):
         if os.path.isfile((os.path.join(rootPath, file))):
             if file.endswith(".py"):
-                print(os.path.join(rootPath, file))
+                filesList.append(os.path.join(rootPath, file))
         else:
             exploreFiles(os.path.join(rootPath, file))
+    return filesList
 
-
-if __name__ == "__main__":
-    exploreFiles("p1")
+# if __name__ == "__main__":
+#    print(exploreFiles("p1"))
